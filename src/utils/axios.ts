88e37eb4b86c1eec776aa8d56f51fa225c8dcb81
Gaurav-r-a-j <gk5271@srmist.edu.in>
2024-08-02 20:05:29 +0530

    updated onrender backend url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5002", // or your deployed backend URL
+  // baseURL: "http://localhost:5002", // or your deployed backend URL
+  baseURL: "https://bajaj-test1-backend.onrender.com", // or your deployed backend URL
 });
 
 export default instance;
